8ff3b40091e1d4ff5bba41e2733b54fdabc54903
Kailash Chander Pathak <you@example.com>
2023-10-08 11:51:36 +0530

    updated the locator

--- tests/uitest.spec.js
+++ tests/uitest.spec.js
@@ -7,8 +7,8 @@ await page.goto('https://talent500.co/auth/signin')
 await page.locator('[name="email"]').click();
 await page.locator('[name="email"]').fill('applitoolsautomation@yopmail.com');
 await page.locator('[name="password"]').fill('Test@123');
-await page.locator("[type='submit']").nth(1).click();
-const text = await page.locator("[id='progress-bar']").textContent();
+await page.locator('[type="submit"]').nth(1).click();
+const text = await page.locator('[id="progress-bar"]').textContent();
 expect(text).toContain("PROFILE");
 });
 });
